96baae76a27176771cc8b8b7cb973d8a28fd06d9
dianramdhani <38197188+dianramdhani@users.noreply.github.com>
2023-08-31 10:05:37 +0700

    Initial commit

---
+++ Demo.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Box ml={{ xs: '56px', md: '56px' }}>
+      <Button onClick={() => setOpen(true)}>left</Button>
+      <Drawer
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={(theme) => ({
+          [theme.breakpoints.up('xs')]: {
+            '&.MuiModal-hidden .MuiDrawer-paper': {
+              width: '56px',
+              overflowX: 'hidden',
+            },
+            '.MuiBackdrop-root': {
+              transition:
+                'opacity 500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms !important',
+            },
+          },
+        })}
+        PaperProps={{
+          sx: (theme) => ({
+            width: 250,
+            [theme.breakpoints.up('xs')]: {
+              visibility: 'visible !important',
+              transform: 'unset !important',
+              whiteSpace: 'nowrap',
+              transition:
+                'width 225ms cubic-bezier(0, 0, 0.2, 1) 0ms !important',
+            },
+          }),
+        }}
+        ModalProps={{ keepMounted: true }}
+      >
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# mui-responsive-sidebar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-97hnql-78tjhy)
